69cf0897cc8ac291806b1ab7f8ecd2afd2a35d54
caecilie99 <birgit.reiter@stud.fh-luebeck.de>
2017-01-03 22:49:42 +0100

    styling event detail

--- src/main/js/components/detail/eventdetail.js
+++ src/main/js/components/detail/eventdetail.js
@@ -25,7 +25,7 @@ export default class EventDetail extends React.Component {
     render() {
         console.log('Render /events/'+this.props.params.id);
         return (
-            <div key={this.props.params.id} className="col-sm-6 col-md-3">
+            <div key={this.props.params.id}>
                 <div  key={this.props.params.id} className="panel panel-default">
 
                     <div className="panel-heading">
@@ -33,8 +33,14 @@ export default class EventDetail extends React.Component {
                         <h5>{Formatter.formatDate(this.state.event.date)}</h5>
                     </div>
                     <div className="panel-body text-left nopadding">
-                        <img src="http://lorempixel.com/200/200"/>
-                        <p>{this.state.event.description}</p>
+                        <div className="media">
+                            <div className="media-left media-middle">
+                                <img className="media-object" src="http://lorempixel.com/200/200"/>
+                            </div>
+                            <div className="media-body">
+                                <h4 className="media-heading">{this.state.event.description}</h4>
+                            </div>
+                        </div>
                     </div>
                     <div className="panel-footer">
                         <h1>Detail</h1>
